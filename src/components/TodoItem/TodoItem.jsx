16b384553244acedc6c5208ed43d05ee8d613eda
Tatyana <catidog25@gmail.com>
2022-07-25 23:57:52 +0700

    Added todoItem

---
+++ src/components/TodoItem/TodoItem.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Check from './Check';
+import cn from 'classname';
+import {BsTrash} from 'react-icons/bs';
+
+const TodoItem = ({todo, changeTodo, removeTodo}) => {
+    return (
+        <div
+            className='flex items-center justify-between mb-4 rounded-2xl bg-gray-800 p-5 w-full'
+        >
+            <button
+                className='flex items-center'
+                onClick={() => changeTodo(todo._id)}
+            >
+                <Check isComplited={todo.isCompleted} />
+                <span
+                    className={cn({
+                        'line-through': todo.isCompleted,
+                    })}
+                >
+                    {todo.title}
+                </span>
+            </button>
+
+            <button onClick={() => removeTodo(todo._id)}>
+                <BsTrash
+                    size={22}
+                    className='text-gray-600
+                    hover:text-red-400
+                    transition-colors easy-in-out duration-300'
+                />
+            </button>
+        </div>
+    );
+};
+
+export default TodoItem;
